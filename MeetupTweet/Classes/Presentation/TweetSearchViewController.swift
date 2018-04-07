7f70ed9f2ecebff58c3bb11fc1b1c412f066aecd
yimajo <y.imajo@gmail.com>
2018-04-07 17:08:02 +0900

    Add SelectWindowUseCase

--- MeetupTweet/Classes/Presentation/TweetSearchViewController.swift
+++ MeetupTweet/Classes/Presentation/TweetSearchViewController.swift
@@ -18,8 +18,7 @@ class TweetSearchViewController: NSViewController {
     @IBOutlet weak var searchField: NSSearchField!
     @IBOutlet weak var stopButton: NSButton!
     @IBOutlet weak var searchButton: NSButton!
-        
-    var window: NSWindow?
+
     let disposeBag = DisposeBag()
     var selectedScreenIndex = 0
 
@@ -84,64 +83,6 @@ extension TweetSearchViewController: NSTableViewDelegate {
         selectedScreenIndex = tableView.selectedRow
         
         let screen = NSScreen.screens[selectedScreenIndex]
-        
-        selectWindow(selectedScreenIndex, screen: screen)
-    }
-}
-
-private extension TweetSearchViewController {
-    
-    func selectWindow(_ index: Int, screen: NSScreen) {
-        
-        window = makeWindow(screen)
-
-        let strongWindow = window!
-        strongWindow.alphaValue = 0.0
-        NSAnimationContext.runAnimationGroup({ context in
-
-                context.duration = 0.25
-                strongWindow.animator().alphaValue = 1.0
-
-            }, completionHandler: {
-                NSAnimationContext.runAnimationGroup({ context in
-
-                    context.duration = 0.25
-                    strongWindow.animator().alphaValue = 0.2
-
-                }, completionHandler: {
-                    strongWindow.orderOut(nil)
-                })
-            }
-        )
-    }
-    
-    func makeWindow(_ screen: NSScreen) -> NSWindow {
-        
-        let frame = NSRect(origin: CGPoint.zero, size: screen.frame.size)
-
-        let window = NSWindow(contentRect: frame, styleMask: NSWindow.StyleMask.resizable, backing: NSWindow.BackingStoreType.buffered, defer: false, screen: screen)
-        
-        window.makeKeyAndOrderFront(nil)
-        
-        setupClearWindow(window)
-        setupMaxWindow(window, screen: screen)
-        
-        return window
-    }
-    
-    func setupClearWindow(_ window: NSWindow) {
-        window.isOpaque = false
-        window.hasShadow = false
-        window.isMovable = true
-        window.isMovableByWindowBackground = true
-        window.isReleasedWhenClosed = false
-        
-        window.backgroundColor = NSColor.black
-    }
-    
-    func setupMaxWindow(_ window: NSWindow, screen: NSScreen) {
-        let screenRect = screen.frame
-        window.setFrame(screenRect, display: true)
-        window.level = NSWindow.Level(Int(CGWindowLevelForKey(.maximumWindow)))
+        SelectWindowUseCase(screen: screen).perform()
     }
 }
