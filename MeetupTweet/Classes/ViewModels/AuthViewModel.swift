cee201e03204eb3d1d60bfe05a2629c42c285b91
yimajo <y.imajo@gmail.com>
2018-10-11 01:15:15 +0900

    コード整形

--- MeetupTweet/Classes/ViewModels/AuthViewModel.swift
+++ MeetupTweet/Classes/ViewModels/AuthViewModel.swift
@@ -17,38 +17,36 @@ class AuthViewModel {
     let validated: Observable<Bool>
     let authorized: Observable<Bool>
     
-    init(consumerKey: Observable<String>, consumerSecret: Observable<String>, authrorizeTap: Observable<Void>) {
+    init(consumerKey: Observable<String>, consumerSecret: Observable<String>, authrorizeTap: Observable<()>) {
         
         let validatedConsumerKey = consumerKey
-            .map {
-                return (0 < $0.count)
-            }
             .shareReplay(1)
         
+            .map { 0 < $0.count }
         let validatedConsumerSecret = consumerSecret
-            .map {
-                return (0 < $0.count)
-            }
-            .shareReplay(1)
-        
-        validated = Observable.combineLatest(validatedConsumerKey, validatedConsumerSecret) { consumerKey, consumerSecret in
-                return consumerKey && consumerSecret
+            .map { 0 < $0.count }
+            .share(replay: 1)
+
+        validated = Observable.combineLatest(validatedConsumerKey,
+                                             validatedConsumerSecret) {
+                $0 && $1
             }
             .distinctUntilChanged()
             .shareReplay(1)
 
         
-        let apiKeyAndSecret = Observable
-            .combineLatest(consumerKey, consumerSecret) {
+        let apiKeyAndSecret = Observable.combineLatest(consumerKey, consumerSecret) {
                 ($0.trimmingCharacters(in: .whitespacesAndNewlines),
                  $1.trimmingCharacters(in: .whitespacesAndNewlines))
             }
         
         authorized = authrorizeTap.withLatestFrom(apiKeyAndSecret)
-            .flatMapLatest { (consumerKey, consumerSecret) -> Observable<Bool>  in
-                UserDefaults.setConsumerKey(consumerKey)
-                UserDefaults.setConsumerSecret(consumerSecret)
-                return AppDelegate.sharedInstance.authorize(consumerKey: consumerKey, consumerSecret: consumerSecret)
+            .flatMapLatest { key, secret -> Observable<Bool>  in
+                UserDefaults.setConsumerKey(key)
+                UserDefaults.setConsumerSecret(secret)
+
+                return AppDelegate.sharedInstance.authorize(consumerKey: key,
+                                                            consumerSecret: secret)
                 
             }
             .shareReplay(1)
