8acd4c1d41ecfe599ea73f1c56c386522bf13aec
yimajo <y.imajo@gmail.com>
2018-12-07 13:23:11 +0900

    Add TestTarget

---
+++ MeetupTweetTests/AuthViewModelTests.swift
@@ -0,0 +1,20 @@
+//
+//  AuthViewModelTests.swift
+//  MeetupTweetTests
+//
+//  Created by Yoshinori Imajo on 2018/12/07.
+//  Copyright © 2018 Yoshinori Imajo. All rights reserved.
+//
+
+import XCTest
+
+class AuthViewModelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
